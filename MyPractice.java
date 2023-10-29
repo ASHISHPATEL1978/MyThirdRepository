0ba1353f85d76bea953360dfb17d5d288635104d
ASHISH PATEL <ASHISHPATELUK@GMAIL.COM>
2023-10-29 15:17:32 +0000

    Third Commit

---
+++ MyPractice.java
@@ -0,0 +1,16 @@
+package practicepackage;
+
+public class MyPractice {
+
+    public static void main(String[] args){
+
+        char A = 50;
+        char B = 10;
+
+        System.out.println( A + B );
+        System.out.println( A / B );
+        System.out.println( A * B );
+
+
+    }
+}
